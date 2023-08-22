db698f6be70de1faee8ad18646b1030626674c3f
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-22 18:51:24 +0000

    fix#5 task0

--- printnumbers.c
+++ printnumbers.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
- *
- *
- *
+ * printnumbers - prints numbers
+ * @string: pointer to number
+ * @type: format specifier
+ * @flag: flag character
+ * @width: width amount
+ * @lengthmod:length modifier
+ * Return: count of printed digits
  */
-int printletters(char *string, char type, char flag, int width, char lengthmod)
+int printnumbers(__attribute__((unused))char *string, __attribute__((unused))char type, __attribute__((unused))char flag, __attribute__((unused))int width, __attribute__((unused))char lengthmod)
 {
 return (0);
 }
