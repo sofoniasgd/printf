58bbab08189872dcc15783338578e3b939e64a03
sofoniasgd <zephaniahsofa@gmail.com>
2023-08-23 18:51:52 +0000

    fix1 task13

--- test/revmain.c
+++ test/revmain.c
@@ -11,20 +11,8 @@ int main(void)
 {
 	int len, len2;
 
-	len = _printf("%r", "\nThis sentence is retrieved from va_args!");
-	len2 = printf("!sgra_av morf deveirter si ecnetnes sihT\n");
-	printf("len=%i\n", len);
-	printf("len2=%i\n", len2);
-	len = _printf("Complete the sentence: You %r nothing, Jon Snow.\n", "know");
+len = _printf("Complete the sentence: You %r nothing, Jon Snow.\n", "know");
 	len2 = printf("Complete the sentence: You wonk nothing, Jon Snow.\n");
-	printf("len=%i\n", len);
-	printf("len2=%i\n", len2);
-	len = _printf("Complete the sentence: You %r nothing, Jon Snow.\n", "");
-	len2 = printf("Complete the sentence: You  nothing, Jon Snow.\n");
-	
-	printf("len=%i\n", len);
-	printf("len2=%i\n", len2);
-	
 	fflush(stdout);
 	if (len != len2)
 	{
